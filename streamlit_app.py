0785c7ee6c33aa3121a14be090cf10947b5f02e7
Fahmi Agung Maulana <121919439+fahmiagungmaulana@users.noreply.github.com>
2023-12-10 12:30:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ st.title(" ")
 col1, col2 = st.columns([1, 4])
 
 with col1:
-    st.image('COVER.png', width=1600, class_='title-img')
+    st.image('COVER.png', width=1600)
 
 with col2:
     st.markdown("<h1 class='title-text'>Rice Classification</h1>", unsafe_allow_html=True)
