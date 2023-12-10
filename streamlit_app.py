bf254b9eac90b2c3b30ac7f93eb49f035c8b0ff2
Fahmi Agung Maulana <121919439+fahmiagungmaulana@users.noreply.github.com>
2023-12-10 11:56:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,30 +9,28 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_i
 st.title("Rice Classification")
 st.write("This classifier categorizes images of rice into five different types: Arborio, Basmati, Ipsala, Jasmine, and Karacadag.")
 
-mobilenet_model = tf.keras.models.load_model('mobilenet.h5')
-
-def predict_mobilenet(uploaded_file):
-    map_dict = {0: 'Arborio', 1: 'Basmati', 2: 'Ipsala', 3: 'Jasmine', 4: 'Karacadag'}
-
-    img = load_img(uploaded_file)
-    input_arr = img_to_array(img)
-    input_arr = np.expand_dims(input_arr, axis=0)
-    input_arr = mobile(input_arr)
-    prediction = mobilenet_model.predict(input_arr)
-    result = map_dict[np.argmax(prediction)]
-    return result
+model = tf.keras.models.load_model('mobilenet.h5')
 
 uploaded_file = st.file_uploader("Choose a image file", type = ['png', 'jpg', 'jpeg'])
-generate_pred = False
+
+map_dict = {
+    0: 'Arborio',
+    1: 'Basmati',
+    2: 'Ipsala',
+    3: 'Jasmine',
+    4: 'Karacadag'
+}
 
 if uploaded_file is not None:
-    generate_pred = st.button("Generate Prediction")
-
-if generate_pred:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-    
+
     st.image(opencv_image, channels="RGB")
-    result = predict_mobilenet(uploaded_file)
-    st.title("The rice is {}".format(result))
+
+    Genrate_pred = st.button("Generate Prediction")
+    if Genrate_pred:
+        img = load_img(uploaded_file)
+        input_arr = img_to_array(img)
+        prediction = model(np.array([input_arr]))
+        st.title("Beras Tersebut adalah beras jenis {}".format(map_dict[np.argmax(prediction)]))
