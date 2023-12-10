e67e21c5843dff00f88c68ac9cc4d04d30879215
Fahmi Agung Maulana <121919439+fahmiagungmaulana@users.noreply.github.com>
2023-12-10 12:48:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.markdown(
     <style>
     .title-container {
         display: flex;
-        align-items: left;
+        align-items: center;
     }
     .title-text {
         margin-top: 20px;
@@ -34,7 +34,7 @@ with col1:
     st.image('COVER.png', width=700)
 
 with col2:
-    st.markdown("<h1 class='title-text'>Rice Classification</h1>", unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align: left;'>Beras Tersebut adalah beras jenis {map_dict[np.argmax(prediction)]}</p>", unsafe_allow_html=True)
 
 st.write("This classifier categorizes images of rice into five different types: Arborio, Basmati, Ipsala, Jasmine, and Karacadag.")
 
