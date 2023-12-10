0ca82150164a3ac4add7ffe7f600fabfaefce36c
Fahmi Agung Maulana <121919439+fahmiagungmaulana@users.noreply.github.com>
2023-12-10 12:59:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,8 @@ st.markdown(
     }
     .title-text {
         margin-top: 20px;
-        margin-right: 0px;
+        margin-left: 10px;
         text-align: left;
-    }
-    .title-img {
-        width: 10px;
-        height: auto;
     }
     </style>
     """,
